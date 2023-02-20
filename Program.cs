5921b23c9636d0cbb41f301717f52bc6f05337d4
WilliamKruse <williamkruse4@gmail.com>
2023-02-20 23:01:09 +0100

    log opgave færdig2

--- Program.cs
+++ Program.cs
@@ -6,7 +6,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Host.ConfigureLogging(logging => 
+ { 
+     logging.ClearProviders(); 
+     logging.AddConsole(); 
+ }); 
+ 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
